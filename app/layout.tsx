bca4bc3cd5822d42dd7701ae88e24adf87e27cd5
umahatokula <umahatokula@gmail.com>
2023-08-01 12:02:40 +0100

    App Layout updated

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,9 +15,11 @@ export default function RootLayout({
 }: {
   children: React.ReactNode;
 }) {
-  return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
-    </html>
-  );
+    return (
+      <html lang="en">
+        <body className={inter.className}>
+          <DefaultLayout>{children}</DefaultLayout>
+        </body>
+      </html>
+    );
 }
