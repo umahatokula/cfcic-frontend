3003413fb981238a79850858f788832daf8931d3
Umaha Tokula <umahatokula@gmail.com>
2023-08-01 11:57:37 +0100

    Font set | Layouts updated

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,12 @@
 import "./globals.css";
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import { Roboto } from "next/font/google";
 
-const inter = Inter({ subsets: ["latin"] });
+const roboto = Roboto({
+  weight: '400',
+  subsets: ['latin'],
+  display: 'swap',
+})
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -14,11 +18,9 @@ export default function RootLayout({
 }: {
   children: React.ReactNode;
 }) {
-    return (
-      <html lang="en">
-        <body className={inter.className}>
-          <DefaultLayout>{children}</DefaultLayout>
-        </body>
-      </html>
-    );
+  return (
+    <html lang="en">
+      <body className={roboto.className}>{children} </body>
+    </html>
+  );
 }
