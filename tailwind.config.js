b9e1d90410bceb19b2c92147f27dc7a3c95bbb5b
umahatokula <umahatokula@gmail.com>
2023-10-15 11:14:35 +0100

    Installed tailwind forms plguin

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,39 +1,39 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     screens: {
-      'tablet': '640px',
+      tablet: "640px",
       // => @media (min-width: 640px) { ... }
 
-      'laptop': '1024px',
+      laptop: "1024px",
       // => @media (min-width: 1024px) { ... }
 
-      'desktop': '1280px',
+      desktop: "1280px",
       // => @media (min-width: 1280px) { ... }
     },
     extend: {
       colors: {
-        'primary': '#03045E',
-        'accent': '#FB8500',
-        'accentDisabled': '#f3be82',
+        primary: "#03045E",
+        accent: "#FB8500",
+        accentDisabled: "#f3be82",
         cfcblue: {
-          100: '#CAF0F8',
-          200: '#48CAE4',
-          300: '#00B4D8',
-          400: '#0077B6',
+          100: "#CAF0F8",
+          200: "#48CAE4",
+          300: "#00B4D8",
+          400: "#0077B6",
         },
       },
       backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic":
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
     },
   },
-  plugins: [require("daisyui")],
-}
+  plugins: [require("daisyui"), require("@tailwindcss/forms")],
+};
